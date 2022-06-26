200d6e203c5040f7423f34d2e49b58c494843549
DanielDinchew <108183002+DanielDinchew@users.noreply.github.com>
2022-06-26 20:05:48 +0300

    right hand rule

--- main.c
+++ main.c
@@ -20,22 +20,22 @@ void right_wall_hugging(int size_row, int size_column, int maze[size_row][size_c
               break;
         }
         if(way == DOWN){
-            if(maze[r][c + 1] == 0){
+            if(maze[r][c - 1] == 0){
                 maze[r][c] = 0;
-                maze[r][c + 1] = 9;
-                c++;
-                way = RIGHT;
+                maze[r][c - 1] = 9;
+                c--;
+                way = LEFT;
             }
             else if(maze[r + 1][c] == 0){
                 maze[r][c] = 0;
                 maze[r + 1][c] = 9;
                 r++;
             }
-            else if(maze[r][c - 1] == 0){
+            else if(maze[r][c + 1] == 0){
                 maze[r][c] = 0;
-                maze[r][c - 1] = 9;
-                c--;
-                way = LEFT;
+                maze[r][c + 1] = 9;
+                c++;
+                way = RIGHT;
             }
             else{
                 maze[r][c] = 0;
@@ -45,23 +45,24 @@ void right_wall_hugging(int size_row, int size_column, int maze[size_row][size_c
             }
         }
         if(way == RIGHT){
-            if(maze[r - 1][c] == 0){
+            if(maze[r + 1][c] == 0){
                 maze[r][c] = 0;
-                maze[r - 1][c] = 9;
-                r--;
-                way = UP;
+                maze[r + 1][c] = 9;
+                r++;
+                way = DOWN;
             }
             else if(maze[r][c + 1] == 0){
                 maze[r][c] = 0;
                 maze[r][c + 1] = 9;
                 c++;
             }
-            else if(maze[r + 1][c]){
+            else if(maze[r - 1][c]){
                 maze[r][c] = 0;
-                maze[r + 1][c] = 9;
-                r++;
-                way = DOWN;
+                maze[r - 1][c] = 9;
+                r--;
+                way = UP;
             }
+
             else{
                 maze[r][c] = 0;
                 maze[r][c - 1] = 9;
@@ -70,22 +71,22 @@ void right_wall_hugging(int size_row, int size_column, int maze[size_row][size_c
             }
         }
         else if(way == UP) {
-            if(maze[r][c - 1] == 0){
+            if(maze[r][c + 1] == 0){
                 maze[r][c] = 0;
-                maze[r][c - 1] = 9;
-                c--;
-                way = LEFT;
+                maze[r][c + 1] = 9;
+                c++;
+                way = RIGHT;
             }
             else if(maze[r - 1][c]==0){
                 maze[r][c] = 0;
                 maze[r - 1][c] = 9;
                 r--;
             }
-            else if(maze[r][c + 1]==0){
+            else if(maze[r][c - 1]==0){
                 maze[r][c] = 0;
-                maze[r][c + 1] = 9;
-                c++;
-                way = RIGHT;
+                maze[r][c - 1] = 9;
+                c--;
+                way = LEFT;
             }
             else{
                 maze[r][c] = 0;
@@ -95,22 +96,22 @@ void right_wall_hugging(int size_row, int size_column, int maze[size_row][size_c
             }
         }
         else if(way == LEFT){
-            if(maze[r + 1][c]==0){
+            if(maze[r - 1][c]==0){
                 maze[r][c] = 0;
-                maze[r + 1][c] = 9;
-                r++;
-                way = DOWN;
+                maze[r - 1][c] = 9;
+                r--;
+                way = UP;
             }
             else if(maze[r][c - 1]==0){
                 maze[r][c] = 0;
                 maze[r][c - 1] = 9;
                 c--;
             }
-            else if(maze[r - 1][c]==0){
+            else if(maze[r + 1][c]==0){
                 maze[r][c] = 0;
-                maze[r - 1][c] = 9;
-                r--;
-                way = UP;
+                maze[r + 1][c] = 9;
+                r++;
+                way = DOWN;
             }
             else{
                 maze[r][c] = 0;
@@ -139,7 +140,7 @@ int main()
     int maze[10][13] = {
     {1,1,1,1,1,1,1,1,1,1,1,1,1},
     {1,0,1,0,1,0,1,0,0,0,0,0,1},
-    {1,0,1,0,0,0,1,0,1,1,1,1,1},
+    {1,0,1,0,0,0,1,0,1,1,1,0,1},
     {1,0,0,0,1,1,1,0,0,0,0,0,1},
     {1,0,1,0,0,0,0,0,1,1,1,0,1},
     {1,0,1,0,1,1,1,0,1,0,0,0,1},
