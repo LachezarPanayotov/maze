8bacc16b3421b90c1e4cd9b40d0b4e806def9a4b
LachezarPanayotov <72467490+LachezarPanayotov@users.noreply.github.com>
2022-06-26 18:11:05 +0300

    почти готово

--- menu.c
+++ menu.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "read_from_file.c"
+#include "traversal_by_user.c"
 
 int main(){
     int choice;
     while(1){
         system("cls");
         printf("1. Read from file\n");
-        //Kazaha mi da napisha traversal (v sluchai che e greshno, NEVINEN SUM)
         printf("2. Traversal by user\n");
         printf("3. Traversal by computer\n");
         printf("4. EXIT\n");
@@ -20,10 +21,18 @@ int main(){
         system("cls");
         switch(choice){
             case 1:
+                read_and_print("maze.txt");
+                printf("\nPress any key to go back to menu...");
+                getch();
                 break;
             case 2 :
+                traversal_by_user(10, 13, maze);
+                printf("\nPress any key to go back to menu...");
+                getch();
                 break;
             case 3:
+                printf("\nPress any key to go back to menu...");
+                getch();
                 break;
             case 4 :
                 exit(0);
